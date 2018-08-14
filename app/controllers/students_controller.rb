8ae7beb50e7a5c2ddf386bee30b7e77c702e425b
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-08-14 18:56:13 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,10 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def activate
+    @student = Student.find(params[:id])
+  end
 
   private
 
